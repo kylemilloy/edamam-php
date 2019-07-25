9760a5c6d5f768f86abf7c59f4b62be5f923a0c7
Kyle Milloy <kmilloy@nubix.ca>
2019-07-24 21:07:32 -0600

    update repointerface

--- src/Edamam/Interfaces/RepositoryInterface.php
+++ src/Edamam/Interfaces/RepositoryInterface.php
@@ -5,6 +5,31 @@ namespace Edamam\Interfaces;
 interface RepositoryInterface
 {
     /**
+     * Process and cast data if necessary.
+     *
+     * @param array $data
+     *
+     * @return array
+     */
+    public function process(array $data);
+
+    /**
+     * Instantiate the instance.
+     *
+     * @param array $data
+     *
+     * @return static
+     */
+    public static function create(array $data = []);
+
+    /**
+     * Return the value from the collection.
+     *
+     * @return \Tightenco\Collect\Support\Collection
+     */
+    public function all(): Collection;
+
+    /**
      * Return the value from the collection.
      *
      * @param string $key
@@ -12,4 +37,18 @@ interface RepositoryInterface
      * @return mixed
      */
     public function get(string $key);
+
+    /**
+     * Cast to array.
+     *
+     * @return array
+     */
+    public function toArray();
+
+    /**
+     * Cast to JSON.
+     *
+     * @return string
+     */
+    public function toJson();
 }
